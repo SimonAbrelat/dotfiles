738d902534a005a4220ab2bd8c40688d413ef6f2
SimonAbrelat <simon.abrelat@gmail.com>
2025-06-22 12:58:22 -0400

    Fixed deploy script to use relative paths

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-ln -s bash/bashrc ~/.bashrc
-ln -s bash ~/.config/bash
-ln -s tmux ~/.config/tmux
+function relink () {
+    if [ -e "$2" ]; then 
+        if [ ! -L "$2" ]; then 
+            echo "CONFLICT: $2 exists and is NOT a symlink"; 
+            exit 1;
+        fi
+        unlink $2
+        echo "INFO: unlinked $2"; 
+    fi
+
+
+    ln -s $(pwd)/$1 $2
+    echo "INFO: deployed $2"; 
+}
+
+# unlink ~/.bashrc
+# unlink ~/.config/bash
+# unlink ~/.config/tmux
+#
+# ln -s $(pwd)/bash/bashrc ~/.bashrc
+# ln -s $(pwd)/bash ~/.config/bash
+# ln -s $(pwd)/tmux ~/.config/tmux
+relink bash/bashrc ~/.bashrc
+relink bash ~/.config/bash
+relink tmux ~/.config/tmux

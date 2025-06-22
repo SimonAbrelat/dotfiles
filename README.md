fde2ea7d87ee99466db55da2ffa22e68a23cecdc
SimonAbrelat <simon.abrelat@gmail.com>
2025-06-22 13:27:40 -0400

    Remove dependency on exa

---
+++ README.md
@@ -0,0 +1,9 @@
+# Dotfiles #
+
+This is the dotfiles for:
+- Bash
+- Tmux
+
+The goal is to have a minimal and self-contained setup for basic bash and tmux: no ZSH, no jumping, no external plugins.
+Rosepine is the color scheme with the ability to easily switch between dark and light themes.
+
--- bash/aliases.sh
+++ bash/aliases.sh
@@ -53,11 +53,10 @@ themes ()
 }
 
 # pretty ls
-alias ls='exa -al --color=always --group-directories-first' # my preferred listing
-alias la='exa -a --color=always --group-directories-first'  # all files and dirs
-alias ll='exa -l --color=always --group-directories-first'  # long format
-alias lt='exa -aT --color=always --group-directories-first' # tree listing
-alias l.='exa -a | grep -E "^\."'
+alias ll='ls -AlXh --group-directories-first --color=auto'
+alias la='ls -A --group-directories-first --color=auto'
+alias lt='ls -AR --group-directories-first --color=auto'
+alias l.='ls -AdXh --group-directories-first --color=auto .*'
 source ~/.config/bash/ls_colors.sh
 
 # navigation
